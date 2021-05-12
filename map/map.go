be6627412c7c39791f9c4c0d33b6779131f43d62
mango555c <wanying_fang@berkeley.edu>
2021-05-12 17:13:05 +0800

    map

---
+++ map/map.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Dictionary map[string]string
+
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+func main() {
+	fmt.Print("lol")
+}
